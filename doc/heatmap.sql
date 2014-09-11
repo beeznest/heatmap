77dc7220d45cc91d1fcb7c6222882428ee08907d
Yannick Warnier <ywarnier@beeznest.org>
2014-09-11 02:36:06 -0500

    Improved SQL

--- doc/heatmap.sql
+++ doc/heatmap.sql
@@ -3,8 +3,8 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `heatmap`;
-CREATE TABLE `heatmap` (
+DROP TABLE IF EXISTS heatmap;
+CREATE TABLE heatmap (
   id int NOT NULL AUTO_INCREMENT,
   page_id int DEFAULT NULL,
   browser_id bigint DEFAULT NULL,
@@ -17,15 +17,14 @@ CREATE TABLE `heatmap` (
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-DROP TABLE IF EXISTS `page`;
-CREATE TABLE `page` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` tinytext NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS page;
+CREATE TABLE page (
+  id int NOT NULL AUTO_INCREMENT,
+  url tinytext NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `page` (`id`, `url`) VALUES
+INSERT INTO page (id, url) VALUES
 (1, 'http://localhost/heatmap/client.html'),
 (2, 'http://vlearning.icpna.edu.pe/in/web/login'),
 (3, 'https://github.com/');
